4056008438dd5ad6c37775b78c68bfbedcc5ab92
MYefimenko <marinka0410@gmail.com>
2023-01-04 22:57:35 +0100

    result

--- src/components/Result.js
+++ src/components/Result.js
@@ -3,10 +3,9 @@ function Result({ correct, questionCount }) {
       <div className="result">
         <img src="./emblem.png" alt="Hogwarts emblem"/>
         <h2>You have {correct} right answers out of {questionCount}</h2>
-        <a href="/">
+        <a href="https://marinayefimenko.github.io/quiz-harry-potter-universe/">
           <button>Try again</button>
         </a>
-
       </div>
     );
   }
